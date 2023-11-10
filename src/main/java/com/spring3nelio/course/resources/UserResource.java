06772c6529ac934f524ed9384ec8ef58d9976570
Elisio Ricardo <elisio.silva@sysmap.com>
2023-11-10 19:32:53 -0300

    adicionando classe notações a classe user e add banco h2

--- src/main/java/com/spring3nelio/course/resources/UserResource.java
+++ src/main/java/com/spring3nelio/course/resources/UserResource.java
@@ -14,8 +14,9 @@ public class UserResource {
 
     @GetMapping
     public ResponseEntity<User> findAll() {
-        User u = new User();
-        return ResponseEntity.ok().body(u);
+        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+        User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+        return ResponseEntity.ok().body(u1);
     }
 
 
